7431f894ae3984ebbc2c5559d0cac20a5fcaed63
Friedy <frd20@pitt.edu>
2022-01-15 20:10:11 -0500

    Begin I2C port

---
+++ drivers/i2c/.i2c-mv64xxx.o.d
@@ -0,0 +1,49 @@
+i2c-mv64xxx.o: drivers/i2c/i2c-mv64xxx.c include/lwk/autoconf.h \
+ drivers/i2c/i2c-mv64xxx.h drivers/i2c/mv643xx_i2c.h include/lwk/types.h \
+ include/lwk/posix_types.h include/lwk/stddef.h include/lwk/compiler.h \
+ include/lwk/compiler-gcc.h include/arch/posix_types.h \
+ include/arch/types.h include/lwk/driver.h include/lwk/kernel.h \
+ /usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdarg.h \
+ include/lwk/macros.h include/lwk/linkage.h include/arch/linkage.h \
+ include/lwk/bitops.h include/arch/bitops.h include/arch/fls.h \
+ include/arch/fls64.h include/arch/ffs.h include/arch/cache.h \
+ include/lwk/kmem.h include/lwk/errno.h include/arch/errno.h \
+ include/arch-generic/errno.h include/arch-generic/errno-base.h \
+ include/lwk/utsname.h include/lwk/print.h include/lwk/log2.h \
+ include/lwk/percpu.h include/lwk/cpu.h include/lwk/string.h \
+ include/arch/string.h include/lwk/cpumask.h include/lwk/bitmap.h \
+ include/arch/percpu.h include/arch/pda.h include/lwk/cache.h \
+ include/arch/page.h include/lwk/const.h \
+ include/arch/pgtable-3level-types.h include/arch/memory.h \
+ include/lwk/sizes.h include/arch/pgtable.h include/arch/pgtable-hwdef.h \
+ include/arch/pgtable-3level-hwdef.h include/arch/pgtable-nopud.h \
+ include/arch/barrier.h include/arch-generic/pgtable.h \
+ include/arch/byteorder.h include/lwk/byteorder/little_endian.h \
+ include/lwk/byteorder/swab.h include/lwk/byteorder/generic.h \
+ include/arch/bug.h include/lwk/stringify.h include/arch-generic/bug.h \
+ include/lwk/params.h include/lwk/init.h include/lwk/pci/pci.h \
+ include/lwk/device.h include/lwk/kobject.h include/lwk/kref.h \
+ ofed/include/asm/atomic.h include/arch/atomic.h include/arch/cmpxchg.h \
+ ofed/include/asm/types.h ofed/include/asm/atomic-long.h \
+ include/lwk/sysfs.h include/lwk/kfs.h include/lwk/list.h \
+ include/lwk/prefetch.h include/arch/processor.h include/arch/segment.h \
+ include/arch/sigcontext.h include/arch/cpufeature.h include/arch/msr.h \
+ include/arch/current.h include/arch/system.h include/lwk/delay.h \
+ include/arch/delay.h include/lwk/reboot.h include/arch/fpsimd.h \
+ include/arch/ptrace.h include/arch/uapi_ptrace.h include/arch/hwcap.h \
+ include/arch/uapi_hwcap.h include/lwk/task.h include/lwk/idspace.h \
+ include/lwk/rbtree.h include/lwk/seqlock.h include/lwk/spinlock.h \
+ include/lwk/spinlock_types.h include/arch/spinlock_types.h \
+ include/arch/spinlock.h include/arch/rwlock.h \
+ include/lwk/spinlock_api_smp.h include/lwk/rlimit.h include/lwk/time.h \
+ include/arch/time.h include/arch/tsc.h include/lwk/signal.h \
+ include/lwk/sigset.h include/arch/signal.h include/arch/siginfo.h \
+ include/arch-generic/siginfo.h include/arch/task.h \
+ include/arch/page_table.h include/arch/mmu.h include/lwk/waitq.h \
+ include/lwk/sched.h include/lwk/sched_rr.h include/lwk/timer.h \
+ include/lwk/mutex.h include/lwk/fdTable.h include/lwk/interrupt.h \
+ include/arch/interrupt.h include/arch/irq_vectors.h \
+ include/lwk/pci/pcicfg.h include/lwk/pci/pcicfg_regs.h \
+ include/arch/fixmap.h include/arch/vsyscall.h include/arch/io.h \
+ include/arch/alternative.h include/arch-generic/iomap.h \
+ include/lwk/blkdev.h
---
+++ drivers/i2c/i2c-mv64xxx.h
@@ -0,0 +1,14 @@
+#ifndef I2C_MV64XXX_H
+#define I2C_MV64XXX_H
+
+#include "mv643xx_i2c.h"
+#include <lwk/driver.h>
+#include <lwk/pci/pci.h>
+#include <lwk/interrupt.h>
+#include <arch/fixmap.h>
+#include <arch/io.h>
+#include <lwk/blkdev.h>
+#include <lwk/delay.h>
+#include <lwk/spinlock.h>
+
+#endif
